465d58ce5922333ef45c1e280d92d09ad39c255f
Robert Williams <roewilli@ucsc.edu>
2021-12-05 14:43:56 -0800

    Made it able to read the csv file. Used github as the source for fetch

--- js/project.js
+++ js/project.js
@@ -1,12 +1,12 @@
 
 
-
 // <a href='https://www.macrotrends.net/countries/USA/united-states/birth-rate'>U.S. Birth Rate 1950-2021</a>
 
 
 const xlabels = [];
 const ylabels = [];
-
+var fileName = "United-states-births.csv";
+//var file = document.querySelector(filename).files[0];
 
 chartIt();
 async function chartIt(){
@@ -25,59 +25,9 @@ async function chartIt(){
       },
   });
 }
-// var lineObj = $.ajax({
-//     "id": 1,
-//    "name": "Contraceptive prevalence: Any method (Percent)",
-//    "shortName": "CPAnyP",
-//    "description": "Percentage of women of reproductive age (15-49 years) who are currently using any method of contraception",
-//    "displayName": "Any",
-//    "dimAge": false,
-//    "dimSex": false,
-//    "dimVariant": true,
-//    "dimCategory": true,
-//    "defaultAgeId": 31,
-//    "defaultSexId": 2,
-//    "defaultVariantId": 4,
-//    "defaultCategoryId": 100,
-//    "variableType": "relative",
-//    "unitScaling": 0.01,
-//    "numberOfDecimals": 1,
-//    "isThousandSeparatorSpace": false,
-//    "formatString": "#0.0",
-//    "unitShortLabel": "%",
-//    "unitLongLabel": "per cent",
-//    "nClassesDefault": 5,
-//    "downloadFileName": "PercentageContraceptive_AnyMethod",
-//    "topicId": 5,
-//    "sourceId": 15,
-//    "topic": null,
-//    "source": {
-//      "id": 15,
-//      "name": "Estimates and Projections of Family Planning Indicators",
-//      "sourceYear": 2020,
-//      "startYear": 1970,
-//      "endYear": 2030,
-//      "citation": "United Nations, Department of Economic and Social Affairs, Population Division (2020). Model-based Estimates and Projections of Family Planning Indicators 2020, custom data acquired via website.",
-//      "url": "http://www.un.org/en/development/desa/population/theme/family-planning/cp_model.asp"
-//         success: function(data) {
-//              // do stuff
-//             console.log(data);
-//             //Variables to get data from the image in simplified form.
-//             var imgUrl = data.img;
-//             var title = data.title;
-//             var alt = data.alt;
-//
-//             //$("#output").text(title);
-//             $("#image").attr("title", title);
-//             $("#output").text(title);
-//
-//             $("#image").attr("src", imgUrl, "alt", alt);
-//
-//             $("#alt").text(alt);
-//           //  $("#alt").text(alt);
-//         },
+
 async function getData(){
-  const response = await fetch('https://population.un.org/dataportal/data/indicators/47/locations/840/start/1990/end/2021/line/linetimeplot/api');
+  response = await fetch('https://raw.githubusercontent.com/robert00000/Art101-Project-Group-2/main/United-states-births.csv');
   const data = await response.text();
   const table = data.split('\n').slice(1);
   table.forEach(row =>{
@@ -87,9 +37,6 @@ async function getData(){
     const birth = columns[1];
     ylabels.push(birth);
     console.log(year, birth);
-
-
-
   })
 }
 //
